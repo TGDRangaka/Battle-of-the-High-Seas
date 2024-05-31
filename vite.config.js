8a371c3f936797e0ed9804eee56e4614e43c8f44
TGDRangaka <tgdilshanrangaka2002@gmail.com>
2024-05-31 21:52:28 +0530

    fix: issue with page redirecting

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import inject from "@rollup/plugin-inject";
+import { resolve } from 'path';
 
 export default defineConfig(
   {
@@ -11,4 +12,12 @@ export default defineConfig(
       })
     ],
     base: '/Battle-of-the-High-Seas/', // 'repo-name'
+    build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'index.html'),
+          game: resolve(__dirname, 'src/pages/game-view.html')
+        }
+      }
+    }
   });
